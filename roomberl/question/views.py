8379e1ec830657905e90d3138ac8648fd6909b7e
Aggrey <39681748+Emmanuel-Aggrey@users.noreply.github.com>
2024-07-04 11:55:17 +0000

    Removed Auth From Questions

--- roomberl/question/views.py
+++ roomberl/question/views.py
@@ -2,11 +2,9 @@
 from question.models import Category
 from question.serializers import CategorySerializer
 from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticated
 
 
 class CategoryApiView(ListAPIView):
-    permission_classes = [IsAuthenticated]
     serializer_class = CategorySerializer
     queryset = Category.objects.order_by("-updated_at")
     pagination_class = None
